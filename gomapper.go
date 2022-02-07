d56c7a748fc8c009b5b942fcd19055cc22005020
Краваль Сергей Иванович <s.kraval@e2e4online.ru>
2022-02-07 23:23:18 +0700

    before test

--- gomapper.go
+++ gomapper.go
@@ -1,7 +1,18 @@
 // gomapper
 package gomapper
 
+import (
+	"sort"
+	"strings"
+)
+
 type Mapper map[string][]string
+
+func Init() *Mapper {
+	m := new(Mapper)
+	*m = make(map[string][]string)
+	return m
+}
 
 // Get gets the first value associated with the given key.
 // If there are no values associated with the key, Get returns
@@ -20,49 +31,49 @@ func (m Mapper) Get(key string) string {
 
 // Set sets the key to value. It replaces any existing
 // values.
-func (v Values) Set(key, value string) {
-	v[key] = []string{value}
+func (m Mapper) Set(key, value string) {
+	m[key] = []string{value}
 }
 
 // Add adds the value to key. It appends to any existing
 // values associated with key.
-func (v Values) Add(key, value string) {
-	v[key] = append(v[key], value)
+func (m Mapper) Add(key, value string) {
+	m[key] = append(m[key], value)
 }
 
 // Del deletes the values associated with key.
-func (v Values) Del(key string) {
-	delete(v, key)
+func (m Mapper) Del(key string) {
+	delete(m, key)
 }
 
 // Has checks whether a given key is set.
-func (v Values) Has(key string) bool {
-	_, ok := v[key]
+func (m Mapper) Has(key string) bool {
+	_, ok := m[key]
 	return ok
 }
 
 // Encode encodes the values into ``URL encoded'' form
 // ("bar=baz&foo=quux") sorted by key.
-func (m Mapper) Implode(glue string) string {
+func (m Mapper) Implode() string {
 	if m == nil {
 		return ""
 	}
 	var buf strings.Builder
-	keys := make([]string, 0, len(v))
-	for k := range v {
+	keys := make([]string, 0, len(m))
+	for k := range m {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
 	for _, k := range keys {
-		vs := v[k]
-		keyEscaped := QueryEscape(k)
-		for _, v := range vs {
+		vs := m[k]
+		//keyEscaped := QueryEscape(k)
+		for _, m := range vs {
 			if buf.Len() > 0 {
 				buf.WriteByte('&')
 			}
-			buf.WriteString(keyEscaped)
+			buf.WriteString(k)
 			buf.WriteByte('=')
-			buf.WriteString(QueryEscape(v))
+			buf.WriteString(m)
 		}
 	}
 	return buf.String()
